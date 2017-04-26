7f328410a7cf8ee1148db07c3d90aa507a607089
Manuel-Rodrigo Cabello Malagon <Manuel-Rodrigo Cabello Malagon>
2017-04-26 08:22:21 +0200

    Initial commit

---
+++ settings.ts
@@ -0,0 +1,8 @@
+export class AppSettings {
+    public static readonly NAMESPACE = 'coreevent.servicebus.windows.net';
+    public static readonly PATH = 'corehybrid';
+    public static readonly KEYRULE = 'RootManageSharedAccessKey';
+    public static readonly KEY = '';
+    public static readonly VIEWSFOLDER = '../../views';
+    public static readonly STATICFOLDERS = '../../public';
+}
